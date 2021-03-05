e30d0a4224ddfc48640a056e4d6534fde271f88c
Bright Chung <bywoch@gmail.com>
2021-03-05 18:00:00 +0900

    add common.js ver3

--- js/common.js
+++ js/common.js
@@ -3,6 +3,60 @@ $(function () {
 
         f = document.forms.select_machine;
 
-        
+        if (obj == 1) {
+
+            f.SUB0.style.display = "none";
+            f.SUB1.style.display = "";
+            f.SUB2.style.display = "none";
+            f.SUB3.style.display = "none";
+            f.SUB4.style.display = "none";
+            f.SUB5.style.display = "none";
+
+        } else if (obj == 2) {
+
+            f.SUB0.style.display = "none";
+            f.SUB1.style.display = "none";
+            f.SUB2.style.display = "";
+            f.SUB3.style.display = "none";
+            f.SUB4.style.display = "none";
+            f.SUB5.style.display = "none";
+
+        } else if (obj == 3) {
+
+            f.SUB0.style.display = "none";
+            f.SUB1.style.display = "none";
+            f.SUB2.style.display = "none";
+            f.SUB3.style.display = "";
+            f.SUB4.style.display = "none";
+            f.SUB5.style.display = "none";
+
+        } else if (obj == 4) {
+
+            f.SUB0.style.display = "none";
+            f.SUB1.style.display = "none";
+            f.SUB2.style.display = "none";
+            f.SUB3.style.display = "none";
+            f.SUB4.style.display = "";
+            f.SUB5.style.display = "none";
+
+        } else if (obj == 5) {
+
+            f.SUB0.style.display = "none";
+            f.SUB1.style.display = "none";
+            f.SUB2.style.display = "none";
+            f.SUB3.style.display = "none";
+            f.SUB4.style.display = "none";
+            f.SUB5.style.display = "";
+
+        } else if (obj == 0) {
+
+            f.SUB0.style.display = "";
+            f.SUB1.style.display = "none";
+            f.SUB2.style.display = "none";
+            f.SUB3.style.display = "none";
+            f.SUB4.style.display = "none";
+
+        }
+        return false
     }
 });
