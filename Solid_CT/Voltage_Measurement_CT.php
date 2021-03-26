15ccfdb84e36b737481b30e92bb5e4cd3f0d2073
Bright Chung <bywoch@gmail.com>
2021-03-26 17:00:00 +0900

    update Solid_CT 3

---
+++ Solid_CT/Voltage_Measurement_CT.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <?php include("../inc/head.php") ?>
+    <link rel="stylesheet" type="text/css" href="../css/Solid_CT/Voltage_Measurement_CT.css">
+</head>
+
+<body>
+    <!--<h1>0</h1>-->
+    <!-- header -->
+    <?php include("../inc/header.php") ?>
+    <!-- //header -->
+    <!-- container -->
+    <container id="container" class="depth_03 vm_ct">
+        <h1 class="blind">container Area</h1>
+        <div class="spl_visual">
+            <div class="visual_box">
+                <div class="visual_txt">
+                    <p>Voltage Measurement CT</p>
+                    <span></span>
+                </div>
+            </div>
+            <div class="prllgr"></div>
+            <div class="spl_txt">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laudantium eius quasi libero dicta iusto, laborum quam quod molestiae, expedita. Accusantium, dignissimos, eveniet! Obcaecati facere, debitis nulla earum itaque, quasi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, eaque atque beatae blanditiis doloribus voluptatibus officiis quidem, nostrum soluta, et mollitia iusto? Magni maxime sit sunt ad, quod voluptatem, dolor.</p>
+            </div>
+        </div>
+        <div class="serch_wrap">
+            <div class="serch_box tab-content">
+                <div class="select_box first-child">
+                    <form action="#" method="get" class="select_form">
+                        <fieldset>
+                            <legend class="blind">제품군</legend>
+                            <p>
+                                <select name="select" id="select" class="select" title="선택해 주세요." onchange="if(this.value) location.href=(this.value);">
+                                    <option value="/Solid_CT/Solid_CT.php">Main</option>
+                                    <option value="/Solid_CT/Revenue_Grade_Metering_CT.php">Revenue Grade Metering CT</option>
+                                    <option value="/Solid_CT/Protection&Monitoring_CT.php">Protection&amp;Monitoring CT</option>
+                                    <option value="/Solid_CT/DC_Immune_CT.php">DC Immune CT</option>
+                                    <option value="/Solid_CT/Voltage_Measurement_CT.php" selected="select">Voltage Measurement CT</option>
+                                    <option value="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">Multi-Harmonics Monitoring CT</option>
+                                    <option value="/Solid_CT/High_Frequency_Monitoring_CT.php">High Frequency Monitoring CT</option>
+                                </select>
+                            </p>
+                        </fieldset>
+                    </form>
+                </div>
+                <div class="select_box">
+                    <form action="#" method="get" class="select_form">
+                        <fieldset>
+                            <legend class="blind">카테고리</legend>
+                            <p>
+                                <select name="select" id="select" class="select" title="선택해 주세요.">
+                                    <option value="" selected="select">카테고리</option>
+                                    <option value="#">옥외용 분할전류센서</option>
+                                    <option value="#">프렉서블 코일</option>
+                                    <option value="#">에너지 하비스트 계전용 센서</option>
+                                    <option value="#">로고스키코일 센서</option>
+                                </select>
+                            </p>
+                        </fieldset>
+                    </form>
+                </div>
+                <div class="select_box">
+                    <form action="#" method="get" class="select_form">
+                        <fieldset>
+                            <legend class="blind">시리즈</legend>
+                            <p>
+                                <select name="select" id="select" class="select" title="선택해 주세요.">
+                                    <option value="" selected="select">시리즈</option>
+                                    <option value="#">옥외용 분할전류센서</option>
+                                    <option value="#">프렉서블 코일</option>
+                                    <option value="#">에너지 하비스트 계전용 센서</option>
+                                    <option value="#">로고스키코일 센서</option>
+                                </select>
+                            </p>
+                        </fieldset>
+                    </form>
+                </div>
+                <div class="search">
+                    <form action="#" method="get" class="serch_bar">
+                        <fieldset>
+                            <legend class="blind">검색</legend>
+                            <p>
+                                <span class="board-sch-keyword-1">
+                                    <input type="text" name="board-sch-keyword" id="board-sch-keyword" title="게시판 검색어" value="" placeholder="모델명을 입력해 주세요">
+                                </span>
+                            </p>
+                        </fieldset>
+                    </form>
+                </div>
+                <div class="sc_button">
+                    <button>검색</button>
+                </div>
+            </div>
+        </div>
+        <div class="nav_wrap">
+            <nav>
+                <ul class="cf">
+                    <li class="nv_m_01"><a href="#">Products</a></li>
+                    <li class="nv_m_02"><a href="#">Features</a></li>
+                    <li class="nv_m_03"><a href="#">Drawing</a></li>
+                    <li class="nv_m_04"><a href="#">Dimension</a></li>
+                    <li class="nv_m_05"><a href="#">FAQ</a></li>
+                    <li class="bgc_00304a"><a href="#">Inquiry</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="prdt_dscrt">
+            <div class="dscrt_txt_wrap">
+                <div class="dscrt_txt_box">
+                    <h2>Voltage Measurement CT</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere atque, nesciunt, laudantium, similique porro alias maxime quod ea, minus est quo sit error quos amet rerum aliquid voluptate placeat magni.</p>
+                    <a href="#">Enquire</a>
+                </div>
+            </div>
+        </div>
+        <div class="od_spl_title">
+            <span></span>
+            <h2>Special Purpose Current Sensors</h2>
+            <div class="od_spl_imgBox">
+                <img src="../img/Solid_CT/Voltage_Measurement_CT/Special Purpose Current Sensors.jpg" alt="Special Purpose Current Sensors">
+            </div>
+            <div style="margin-top: 80px;">
+                <p><strong>Application</strong><br><br>
+                    - Meeting International Standard IEC 61869-2<br>
+                    - MTransmission &amp; Distribution Line Sensors<br>
+                    - Current Transformer with Voltage Monitoring<br>
+                    - High-End Isolation Voltage Transformer<br>
+                    - Super Accuracy Current Transformer<br>
+                    - Track-type Zero Phase CT<br>
+                    - Over Current Protection Relay<br>
+                    - Load Limit Hoist<br>
+                    - Automatic Section Switch (ASS)<br>
+                    - RTU Pole-Top Transformer<br><br><br>
+                </p>
+            </div>
+        </div>
+        <div class="tqp_srs_wrap">
+            <div id="tqp_srs_wrap" class="tqp_srs_box srs_bx_01 cf">
+                <div class="tqp_srs_txt">
+                    <div class="srs_txt_title src_title_01 cf">
+                        <h4>Drawing</h4>
+                        <span></span>
+                    </div>
+                    <div class="srs_txt_p srs_txt_01">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus suscipit ab odio. Dolorem consequatur minus est temporibus libero perferendis quis veniam? Quae consectetur labore animi reprehenderit. Qui et dignissimos, ipsa.</p>
+                    </div>
+                </div>
+                <div class="tqp_srs_img srs_img_01">
+                    <img src="../img/Solid_CT/Voltage_Measurement_CT/Supreme-Accuracy-T&D-Line-Sensor-specifigation2-drawing.png" alt="Supreme-Accuracy-T&D-Line-Sensor-specifigation-Drawing">
+                </div>
+                <div class="tqp_srs_img srs_img_01 fl_r">
+                    <!--<p>Standard Accuracy : Class 0.5</p>-->
+                    <img src="../img/Solid_CT/Voltage_Measurement_CT/Supreme-Accuracy-T&D-Line-Sensor-specifigation2-drawing_2.png" alt="Supreme-Accuracy-T&D-Line-Sensor-specifigation-Drawing">
+                </div>
+            </div>
+        </div>
+        <div class="od_spl_title">
+            <h2>Dimension</h2>
+            <span></span>
+            <!--<p><strong>Standard Accuracy : 3.0 Class</strong></p>-->
+        </div>
+        <div id="spcfct_wrap" class="spcfct_wrap">
+            <div class="spcfct_box">
+                <!--<p>Specification</p>-->
+                <div class="tabs">
+                    <button class="tab_btn tab_btn_left"></button>
+                    <button class="tab_btn tab_btn_right"></button>
+                    <div class="tabs-content">
+                        <div tab-id="1" class="tab active">
+                            <table border="1">
+                                <caption class="blind">Specification Spec</caption>
+                                <thead></thead>
+                                <tbody>
+                                    <tr>
+                                        <th rowspan="2" class="table_head">Model NO</th>
+                                        <th rowspan="2" class="table_head">Current Ratio</th>
+                                        <th colspan="2" class="table_head">Ratio Error &amp; Phase shift at 1mA, 110V</th>
+                                        <th class="table_head">DCR(Ω)</th>
+                                        <th colspan="4" class="table_head">Dimensions(mm)</th>
+                                    </tr>
+                                    <tr>
+                                        <td class="table_head">Ratio Error (%)</td>
+                                        <td class="table_head">Phase Shift (min)</td>
+                                        <td class="table_head">(±6%)</td>
+                                        <td class="table_head">Window</td>
+                                        <td class="table_head">Width</td>
+                                        <td class="table_head">Height</td>
+                                        <td class="table_head">Thickness</td>
+                                    </tr>
+                                    <tr>
+                                        <td>TC159V</td>
+                                        <td>11,000 : 150</td>
+                                        <td>-0.03 ± 0.07</td>
+                                        <td>63 ± 5'</td>
+                                        <td>840, 0.8</td>
+                                        <td>17.5</td>
+                                        <td>55.7</td>
+                                        <td>64.5</td>
+                                        <td>24.0 </td>
+                                    </tr>
+                                    <tr>
+                                        <td>TC162V</td>
+                                        <td>500 : 170</td>
+                                        <td>-0.03 ± 0.07</td>
+                                        <td>4 ± 4'</td>
+                                        <td>13, 2.2</td>
+                                        <td>22.1</td>
+                                        <td>45.0</td>
+                                        <td>42.2</td>
+                                        <td>24.2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>TZ111V</td>
+                                        <td>1250 : 1250</td>
+                                        <td>-0.03 ± 0.07</td>
+                                        <td>27 ± 5'</td>
+                                        <td>100</td>
+                                        <td>6.9</td>
+                                        <td>24.5</td>
+                                        <td>25.0</td>
+                                        <td>11.0</td>
+                                    </tr>
+                                    <tr>
+                                        <td>TZ112V</td>
+                                        <td>2500 :  2500</td>
+                                        <td>-0.03 ± 0.07</td>
+                                        <td>15 ± 5'</td>
+                                        <td>245</td>
+                                        <td>12.9</td>
+                                        <td>38.0</td>
+                                        <td>39.3</td>
+                                        <td>14.0</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <p>* V : PCB Mountable<br>* L : Lead Wire</p>
+                        </div>
+                    </div>
+                </div>
+                <link rel="stylesheet" type="text/css" href="../css/jquery.scrolling-tabs.css">
+                <script type="text/javascript" src="../js/jquery.scrolling-tabs.js"></script>
+            </div>
+        </div>
+        
+        <div class="od_spl_title tt_w_l tt_w_02">
+            <span></span>
+            <h2>FAQ</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, aut, quidem! Fugit debitis, eos ad sapiente consequuntur doloribus, laudantium, dolor incidunt iusto nisi repellendus distinctio quis? Commodi explicabo, labore suscipit.</p>
+        </div>
+        <div class="pp_as_ask_wrap">
+            <nav class="pp_as_ask_box">
+                <h3>Lorem ipsum</h3>
+                <ul>
+                    <li>
+                        <a href="">Lorem ipsum <span class="css-arrow"></span></a>
+                        <nav class="pp_as_sub">
+                            <ul>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                            </ul>
+                        </nav>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum <span class="css-arrow"></span></a>
+                        <nav class="pp_as_sub">
+                            <ul>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                            </ul>
+                        </nav>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum <span class="css-arrow"></span></a>
+                        <nav class="pp_as_sub">
+                            <ul>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                            </ul>
+                        </nav>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum <span class="css-arrow"></span></a>
+                        <nav class="pp_as_sub">
+                            <ul>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                            </ul>
+                        </nav>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum <span class="css-arrow"></span></a>
+                        <nav class="pp_as_sub">
+                            <ul>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                            </ul>
+                        </nav>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum <span class="css-arrow"></span></a>
+                        <nav class="pp_as_sub">
+                            <ul>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                                <li><a href="">Lorem ipsum dolor sit amet</a></li>
+                            </ul>
+                        </nav>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="od_spl_title tt_w_l faq_title">
+            <span></span>
+            <h2>Inquiry</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore nihil excepturi vitae doloribus, nostrum ratione nesciunt, totam porro distinctio esse voluptates iusto sed perspiciatis commodi accusantium voluptatem doloremque.</p>
+            <a href="/index.php">Contacts</a>
+        </div>
+    </container>
+    <!-- //container -->
+    <!-- footer -->
+    <?php include("../inc/footer.php") ?>
+    <!-- //footer -->
+    <?php include("../inc/btn-up.php") ?>
+</body>
+
+</html>
