a5dd39d807b5b77ec86df500b788486e100fcb7c
Bright Chung <bywoch@gmail.com>
2021-05-07 15:00:00 +0900

    update index.html 2

--- index.php
+++ index.php
@@ -29,15 +29,13 @@
         <div class="select_wrap">
             <div class="search_allbox">
                 <div class="search_title cf">
-                    <div class="sc_tt_01 active" data-tab="tab-1" style="width: 70%;">
+                    <div class="sc_tt_01 active" data-tab="tab-1">
                         <p class="tt_01_l_txt ds_n_768"><strong>Search</strong> | Search for the right product</p>
-                        <p class="tt_01_s_txt ds_n_768" style="display: none;">Search</p>
+                        <p class="tt_01_s_txt ds_n_768">Search</p>
                         <p class="sc_tt_01_768 ds_bl_768">Search</p>
                     </div>
-                    <div class="sc_tt_02 ds_n_768" data-tab="tab-2" style="width: 30%;">
-                        <p class="tt_02_l_txt" style="display: none;">Taehwatrans for all of you |
-                            <strong>Information</strong>
-                        </p>
+                    <div class="sc_tt_02 ds_n_768" data-tab="tab-2">
+                        <p class="tt_02_l_txt" style="display: none;">Taehwatrans for all of you | <strong>Information</strong></p>
                         <p class="tt_02_s_txt">Information</p>
                     </div>
                 </div>
@@ -51,8 +49,7 @@
                                 <p>Product</p>
                                 <legend class="blind">Product</legend>
                                 <p>
-                                    <select name=" " class="select"
-                                        onchange="showSub(this.options[this.selectedIndex].value);">
+                                    <select name=" " class="select" onChange="showSub(this.options[this.selectedIndex].value);">
                                         <option value="0" selected="select">Product</option>
                                         <option value="1">Solid CTs for Metering &amp; monitoring</option>
                                         <option value="2">Split-core CTs</option>
@@ -67,63 +64,40 @@
                                 <p>Category</p>
                                 <legend class="blind">Category</legend>
                                 <p>
-                                    <select name="SUB0" class="select" style="">
-                                        <option value="">------------------------------ Category
-                                            ------------------------------</option>
+                                    <select name="SUB0" class="select" style="display: ;">
+                                        <option value="">------------------------------ Category ------------------------------</option>
                                     </select>
-                                    <select name="SUB1" class="select1 select" style="display: none;"
-                                        onchange="if(this.value) location.href=(this.value);">
-                                        <option value="">------------------------------ Category
-                                            ------------------------------</option>
+                                    <select name="SUB1" class="select1 select" style="display: none;" onchange="if(this.value) location.href=(this.value);">
+                                        <option value="">------------------------------ Category ------------------------------</option>
                                         <option value="/Solid_CT/Solid_CT.php">Main</option>
-                                        <option value="/Solid_CT/Revenue_Grade_Metering_CT.php">Revenue Grade Metering
-                                            CT</option>
-                                        <option value="/Solid_CT/Protection&amp;Monitoring_CT.php">
-                                            Protection&amp;Monitoring CT</option>
+                                        <option value="/Solid_CT/Revenue_Grade_Metering_CT.php">Revenue Grade Metering CT</option>
+                                        <option value="/Solid_CT/Protection&Monitoring_CT.php">Protection&amp;Monitoring CT</option>
                                         <option value="/Solid_CT/DC_Immune_CT.php">DC Immune CT</option>
-                                        <option value="/Solid_CT/Voltage_Measurement_CT.php">Voltage Measurement CT
-                                        </option>
-                                        <option value="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">Multi-Harmonics
-                                            Monitoring CT</option>
-                                        <option value="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">High Frequency
-                                            Monitoring CT</option>
+                                        <option value="/Solid_CT/Voltage_Measurement_CT.php">Voltage Measurement CT</option>
+                                        <option value="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">Multi-Harmonics Monitoring CT</option>
+                                        <option value="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">High Frequency Monitoring CT</option>
                                     </select>
-                                    <select name="SUB2" class="select1 select" style="display: none;"
-                                        onchange="if(this.value) location.href=(this.value);">
-                                        <option value="">------------------------------ Category
-                                            ------------------------------</option>
+                                    <select name="SUB2" class="select1 select" style="display: none;" onchange="if(this.value) location.href=(this.value);">
+                                        <option value="">------------------------------ Category ------------------------------</option>
                                         <option value="/Split-core_CT/Split-core_CT.php">Main</option>
-                                        <option value="/Split-core_CT/Miniature-Split-core_CT.php">Miniature Split-core
-                                            CT</option>
-                                        <option value="/Split-core_CT/Busbar-Split-core_CT.php">Busbar Split-core CT
-                                        </option>
-                                        <option value="/Split-core_CT/Outdoor-Split-Core-transformer.php">Outdoor
-                                            Split-Core CT</option>
+                                        <option value="/Split-core_CT/Miniature-Split-core_CT.php">Miniature Split-core CT</option>
+                                        <option value="/Split-core_CT/Busbar-Split-core_CT.php">Busbar Split-core CT</option>
+                                        <option value="/Split-core_CT/Outdoor-Split-Core-transformer.php">Outdoor Split-Core CT</option>
                                     </select>
-                                    <select name="SUB3" class="select1 select" style="display: none;"
-                                        onchange="if(this.value) location.href=(this.value);">
-                                        <option value="">------------------------------ Category
-                                            ------------------------------</option>
+                                    <select name="SUB3" class="select1 select" style="display: none;" onchange="if(this.value) location.href=(this.value);">
+                                        <option value="">------------------------------ Category ------------------------------</option>
                                         <option value="/Zero-Phase_CT/Zero-Phase_CT.php">Zero Phase CT</option>
                                     </select>
-                                    <select name="SUB4" class="select1 select" style="display: none;"
-                                        onchange="if(this.value) location.href=(this.value);">
-                                        <option value="">------------------------------ Category
-                                            ------------------------------</option>
+                                    <select name="SUB4" class="select1 select" style="display: none;" onchange="if(this.value) location.href=(this.value);">
+                                        <option value="">------------------------------ Category ------------------------------</option>
                                         <option value="/Rogowski_Coils/Rogowski_Coils.php">Main</option>
-                                        <option value="/Rogowski_Coils/Rigid_Rogowski_Coils.php">Rigid Rogowski Coils
-                                        </option>
-                                        <option value="/Rogowski_Coils/Flexible_Rogowski_Coils.php">Flexible Rogowski
-                                            Coils</option>
-                                        <option value="/Rogowski_Coils/MV_Flexible_Rogowski_Coils.php">MV Flexible
-                                            Rogowski Coils</option>
-                                        <option value="/Rogowski_Coils/Clamp-on_Rogowski_Coils.php">Clamp on Rogowski
-                                            Coils</option>
+                                        <option value="/Rogowski_Coils/Rigid_Rogowski_Coils.php">Rigid Rogowski Coils</option>
+                                        <option value="/Rogowski_Coils/Flexible_Rogowski_Coils.php">Flexible Rogowski Coils</option>
+                                        <option value="/Rogowski_Coils/MV_Flexible_Rogowski_Coils.php">MV Flexible Rogowski Coils</option>
+                                        <option value="/Rogowski_Coils/Clamp-on_Rogowski_Coils.php">Clamp on Rogowski Coils</option>
                                     </select>
-                                    <select name="SUB5" class="select1 select" style="display: none;"
-                                        onchange="if(this.value) location.href=(this.value);">
-                                        <option value="">------------------------------ Category
-                                            ------------------------------</option>
+                                    <select name="SUB5" class="select1 select" style="display: none;" onchange="if(this.value) location.href=(this.value);">
+                                        <option value="">------------------------------ Category ------------------------------</option>
                                         <option value="/ACB_CTs/ACB_CTs.php">ACB &amp; GIS Current Transformer</option>
                                     </select>
                                 </p>
@@ -136,11 +110,73 @@
 
                         </form>
                     </div>
+                    <script type="text/javascript">
+                        function showSub(obj) {
+
+                            f = document.forms.select_machine;
+
+                            if (obj == 1) {
+
+                                f.SUB0.style.display = "none";
+                                f.SUB1.style.display = "";
+                                f.SUB2.style.display = "none";
+                                f.SUB3.style.display = "none";
+                                f.SUB4.style.display = "none";
+                                f.SUB5.style.display = "none";
+
+                            } else if (obj == 2) {
+
+                                f.SUB0.style.display = "none";
+                                f.SUB1.style.display = "none";
+                                f.SUB2.style.display = "";
+                                f.SUB3.style.display = "none";
+                                f.SUB4.style.display = "none";
+                                f.SUB5.style.display = "none";
+
+                            } else if (obj == 3) {
+
+                                f.SUB0.style.display = "none";
+                                f.SUB1.style.display = "none";
+                                f.SUB2.style.display = "none";
+                                f.SUB3.style.display = "";
+                                f.SUB4.style.display = "none";
+                                f.SUB5.style.display = "none";
+
+                            } else if (obj == 4) {
+
+                                f.SUB0.style.display = "none";
+                                f.SUB1.style.display = "none";
+                                f.SUB2.style.display = "none";
+                                f.SUB3.style.display = "none";
+                                f.SUB4.style.display = "";
+                                f.SUB5.style.display = "none";
+
+                            } else if (obj == 5) {
+
+                                f.SUB0.style.display = "none";
+                                f.SUB1.style.display = "none";
+                                f.SUB2.style.display = "none";
+                                f.SUB3.style.display = "none";
+                                f.SUB4.style.display = "none";
+                                f.SUB5.style.display = "";
+
+                            } else if (obj == 0) {
+
+                                f.SUB0.style.display = "";
+                                f.SUB1.style.display = "none";
+                                f.SUB2.style.display = "none";
+                                f.SUB3.style.display = "none";
+                                f.SUB4.style.display = "none";
+
+                            }
+                            return false
+                        }
+                    </script>
                     <div class="sc_button" style="display: none;">
                         <button type="button" onclick="location.href='' ">Choose</button>
                     </div>
                 </div>
-                <div class="md_slt_box tab-content" style="display: none;">
+                <div class="md_slt_box tab-content">
                     <div class="md_slt_ctt bd_l_n cf">
                         <div class="md_slt_txt">
                             <a href="/aboutus">About us</a>
@@ -215,7 +251,7 @@
                     </a>
                 </li>
                 <li class="contents_box ml_n_480">
-                    <a href="/Solid_CT/Protection&amp;Monitoring_CT.php" class="contents">
+                    <a href="/Solid_CT/Protection&Monitoring_CT.php" class="contents">
                         <div class="ctt_img">
                             <img src="/img/main_ctt_img_02.jpg" alt="ctt_img">
                         </div>
@@ -260,7 +296,7 @@
                 </li>
             </ul>
         </div>
-        <div class="all_box cf">
+        <div class="all_box h_100 cf">
             <div class="contents_title">
                 <h2>Split-core CT</h2>
                 <span></span>
@@ -298,7 +334,7 @@
                 </li>
             </ul>
         </div>
-        <div class="all_box cf">
+        <div class="all_box h_100 cf">
             <div class="contents_title">
                 <h2>Zero Phase CT<br>(ZCT)</h2>
                 <span></span>
@@ -316,7 +352,7 @@
                 </li>
             </ul>
         </div>
-        <div class="all_box cf">
+        <div class="all_box h_100 cf">
             <div class="contents_title">
                 <h2>Rogowski Coils</h2>
                 <span></span>
@@ -364,7 +400,7 @@
                 </li>
             </ul>
         </div>
-        <div class="all_box cf">
+        <div class="all_box h_100 cf">
             <div class="contents_title">
                 <h2>ACB &amp; GIS Current Transformer</h2>
                 <span></span>
@@ -391,66 +427,3 @@
 </body>
 
 </html>
-
-<script type="text/javascript">
-    function showSub(obj) {
-
-        f = document.forms.select_machine;
-
-        if (obj == 1) {
-
-            f.SUB0.style.display = "none";
-            f.SUB1.style.display = "";
-            f.SUB2.style.display = "none";
-            f.SUB3.style.display = "none";
-            f.SUB4.style.display = "none";
-            f.SUB5.style.display = "none";
-
-        } else if (obj == 2) {
-
-            f.SUB0.style.display = "none";
-            f.SUB1.style.display = "none";
-            f.SUB2.style.display = "";
-            f.SUB3.style.display = "none";
-            f.SUB4.style.display = "none";
-            f.SUB5.style.display = "none";
-
-        } else if (obj == 3) {
-
-            f.SUB0.style.display = "none";
-            f.SUB1.style.display = "none";
-            f.SUB2.style.display = "none";
-            f.SUB3.style.display = "";
-            f.SUB4.style.display = "none";
-            f.SUB5.style.display = "none";
-
-        } else if (obj == 4) {
-
-            f.SUB0.style.display = "none";
-            f.SUB1.style.display = "none";
-            f.SUB2.style.display = "none";
-            f.SUB3.style.display = "none";
-            f.SUB4.style.display = "";
-            f.SUB5.style.display = "none";
-
-        } else if (obj == 5) {
-
-            f.SUB0.style.display = "none";
-            f.SUB1.style.display = "none";
-            f.SUB2.style.display = "none";
-            f.SUB3.style.display = "none";
-            f.SUB4.style.display = "none";
-            f.SUB5.style.display = "";
-
-        } else if (obj == 0) {
-
-            f.SUB0.style.display = "";
-            f.SUB1.style.display = "none";
-            f.SUB2.style.display = "none";
-            f.SUB3.style.display = "none";
-            f.SUB4.style.display = "none";
-
-        }
-        return false
-    }
-</script>
