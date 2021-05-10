cd500764a7e7c8dadbe7a8ea84401e1324e699ca
Bright Chung <bywoch@gmail.com>
2021-05-10 11:00:00 +0900

    update Solid_CT

--- Solid_CT/Solid_CT.php
+++ Solid_CT/Solid_CT.php
@@ -13,10 +13,9 @@
     <!-- //header -->
     <!-- container -->
     <container id="container" class="split-core_ct">
-        <div class="spl_serch_wrap">
-            <div class="spl_serch_box tab-content">
+        <div class="spl_search_wrap">
+            <div class="spl_search_box tab-content">
                 <div class="spl_select_box first-child cf">
-                    <!--<p>제품군</p>-->
                     <a class="icon_home" href="/index.php">
                         <img src="/img/icon-home.png" alt="홈 버튼">
                     </a>
@@ -24,12 +23,15 @@
                         <fieldset>
                             <legend class="blind">제품군</legend>
                             <p>
-                                <select name="select" id="select" class="spl_select" title="선택해 주세요." onchange="if(this.value) location.href=(this.value);">
-                                    <option value="/Solid_CT/Solid_CT.php" selected="select">Solid CT for Energy Meters</option>
+                                <select name="select" id="select"
+                                    style="width: 270px; height: 44px; float: left; color: #666; padding: 7px;"
+                                    title="선택해 주세요." onchange="if(this.value) location.href=(this.value);">
+                                    <option value="/Solid_CT/Solid_CT.php" selected="select">Solid CTs for Metering
+                                        &amp; monitoring</option>
                                     <option value="/Split-core_CT/Split-core_CT.php">Split-core CT</option>
-                                    <option value="/Rogowski_Coils/Rogowski_Coils.php">Rogowski Coils</option>
                                     <option value="/Zero-Phase_CT/Zero-Phase_CT.php">Zero Phase CT</option>
-                                    <option value="/Custom_Current_Sensors/Custom_Current_Sensors.php">Custom Current Sensors</option>
+                                    <option value="/Rogowski_Coils/Rogowski_Coils.php">Rogowski Coils</option>
+                                    <option value="/ACB_CTs/ACB_CTs.php">ACB &amp; GIS Current Transformer</option>
                                 </select>
                             </p>
                         </fieldset>
@@ -39,14 +41,20 @@
                         <fieldset>
                             <legend class="blind">카테고리</legend>
                             <p>
-                                <select name="select" id="select" class="spl_select" title="선택해 주세요." onchange="if(this.value) location.href=(this.value);">
+                                <select name="select" id="select"
+                                    style="width: 270px; height: 44px; float: left; color: #666; padding: 7px;"
+                                    title="선택해 주세요." onchange="if(this.value) location.href=(this.value);">
                                     <option value="/Solid_CT/Solid_CT.php" selected="select">Main</option>
-                                    <option value="/Solid_CT/Revenue_Grade_Metering_CT.php">Revenue Grade Metering CT</option>
-                                    <option value="/Solid_CT/Protection&Monitoring_CT.php">Protection&amp;Monitoring CT</option>
+                                    <option value="/Solid_CT/Revenue_Grade_Metering_CT.php">Revenue Grade Metering CT
+                                    </option>
+                                    <option value="/Solid_CT/Protection&Monitoring_CT.php">Protection&amp;Monitoring CT
+                                    </option>
                                     <option value="/Solid_CT/DC_Immune_CT.php">DC Immune CT</option>
                                     <option value="/Solid_CT/Voltage_Measurement_CT.php">Voltage Measurement CT</option>
-                                    <option value="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">Multi-Harmonics Monitoring CT</option>
-                                    <option value="/Solid_CT/High_Frequency_Monitoring_CT.php">High Frequency Monitoring CT</option>
+                                    <option value="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">Multi-Harmonics
+                                        Monitoring CT</option>
+                                    <option value="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">High Frequency
+                                        Monitoring CT</option>
                                 </select>
                             </p>
                         </fieldset>
@@ -57,20 +65,34 @@
         <div class="spot_solct_allbox">
             <div class="spot_solct_box">
                 <div class="spot_txt_box">
-                    <h3>Solid CT for Energy Meters</h3><br>
-                    <p>Outdoor split core series includes 5A, 1A, and mA secondary outputfor varieties of user application</p>
+                    <h3>Solid CTs for Metering &amp; monitoring</h3><br>
+                    <!--<p>Outdoor split core series includes 5A, 1A, and mA secondary outputfor varieties of user application</p>-->
                 </div>
             </div>
+        </div>
+        <div class="dep02_introduce_txt">
+            <p>Taehwatrans offers an extensive line of toroidal current transformers for power metering and monitoring
+                applications. Product portfolio includes revenue grade metering CTs, DC immune CTs, Protection &amp;
+                Monitoring CTs, High Frequency Monitoring CTs, Multi-Harmonics Monitoring CTs and Voltage Measurement
+                CTs. This vertically deep and horizontally wide categories of CTs are attributed by Taehwatrans’ 40
+                years of application engineering. Our products are UL Recognized for both US and Canada and Measurement
+                Canada certified. RoHs compliant. They are epoxy encapsulated to provide protection from the
+                environment.<br><br>Supreme Accuracy current transformers consist of a variety of CTs, for example, HVCT
+                / MVCT Revenue Grade Metering CTs, Multi Harmonics CTs, Smart Metering CTs, Isolation CTs and Arc Plasma
+                CTs. They are widely utilized in 0.1/0.2 class meters and different types of energy measurement devices
+                that require the highest accuracy class in the market. Taehwatrans is a power house capable of providing
+                all sizes of current transformers for a variety of smart grid applications.</p>
         </div>
         <div class="content_sol_allbox cf">
             <div class="content_sol_box ml_0">
                 <div class="sol_padding_box">
                     <div class="sol_img sol_ct_01">
-                        <a href="/Solid_CT/Revenue_Grade_Metering_CT.php"><img src="/img/ctt_img_01.png" alt="ctt_img"></a>
+                        <a href="/Solid_CT/Revenue_Grade_Metering_CT.php"><img src="/img/scem_ctt_img_01_220720.jpg"
+                                alt="ctt_img"></a>
                     </div>
                     <div class="sol_txt">
-                        <h4>Revenue Grade Metering CT</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h4>Revenue Grade Metering CTs</h4>
+                        <p>0.1 and 0.2 Class CTs with excellent linearity and low current sensitivity.</p>
                     </div>
                     <div class="sol_btn_box">
                         <a class="sol_btn" href="/Solid_CT/Revenue_Grade_Metering_CT.php">VIEW MORE</a>
@@ -80,11 +102,12 @@
             <div class="content_sol_box ml_480_n">
                 <div class="sol_padding_box">
                     <div class="sol_img sol_ct_02">
-                        <a href="/Solid_CT/Protection&Monitoring_CT.php"><img src="/img/ctt_img_02.png" alt="ctt_img"></a>
+                        <a href="/Solid_CT/Protection&Monitoring_CT.php"><img src="/img/scem_ctt_img_02_220720.jpg"
+                                alt="ctt_img"></a>
                     </div>
                     <div class="sol_txt">
-                        <h4>Protection &amp; Monitoring CT</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h4>Protection &amp; Monitoring CTs</h4>
+                        <p>Supreme accuracy for Monitoring &amp; Relay.</p>
                     </div>
                     <div class="sol_btn_box">
                         <a class="sol_btn" href="/Solid_CT/Protection&Monitoring_CT.php">VIEW MORE</a>
@@ -94,11 +117,12 @@
             <div class="content_sol_box ml_1024_n">
                 <div class="sol_padding_box">
                     <div class="sol_img sol_ct_03">
-                        <a href="/Solid_CT/DC_Immune_CT.php"><img src="/img/ctt_img_03.png" alt="ctt_img"></a>
+                        <a href="/Solid_CT/DC_Immune_CT.php"><img src="/img/scem_ctt_img_03_220720.jpg"
+                                alt="ctt_img"></a>
                     </div>
                     <div class="sol_txt">
-                        <h4>DC Immune C</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h4>DC Immune CTs</h4>
+                        <p>Metering of DC component and AC current .</p>
                     </div>
                     <div class="sol_btn_box">
                         <a class="sol_btn" href="/Solid_CT/DC_Immune_CT.php">VIEW MORE</a>
@@ -108,11 +132,12 @@
             <div class="content_sol_box ml_0 ml_1024_2p ml_480_n">
                 <div class="sol_padding_box">
                     <div class="sol_img sol_ct_04">
-                        <a href="/Solid_CT/Voltage_Measurement_CT.php"><img src="/img/ctt_img_04.png" alt="ctt_img"></a>
+                        <a href="/Solid_CT/Voltage_Measurement_CT.php"><img src="/img/scem_ctt_img_04.jpg"
+                                alt="ctt_img"></a>
                     </div>
                     <div class="sol_txt">
-                        <h4>Voltage Measurement CT</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h4>Voltage Measurement CTs</h4>
+                        <p>Best-in-class accuracy with winding isolation.</p>
                     </div>
                     <div class="sol_btn_box">
                         <a class="sol_btn" href="/Solid_CT/Voltage_Measurement_CT.php">VIEW MORE</a>
@@ -122,28 +147,15 @@
             <div class="content_sol_box ml_1024_n">
                 <div class="sol_padding_box">
                     <div class="sol_img sol_ct_05">
-                        <a href="/Solid_CT/Multi-Harmonics_Monitoring_CT.php"><img src="/img/ctt_img_05.png" alt="ctt_img"></a>
+                        <a href="/Solid_CT/Multi-Harmonics_Monitoring_CT.php"><img src="/img/scem_ctt_img_05.jpg"
+                                alt="ctt_img"></a>
                     </div>
                     <div class="sol_txt">
-                        <h4>Multi-Harmonics Monitoring CT</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h4>Multi-Harmonics/ High Frequency Monitoring CTs</h4>
+                        <p>BAccurate multiple harmonics and high frequency measurement.</p>
                     </div>
                     <div class="sol_btn_box">
                         <a class="sol_btn" href="/Solid_CT/Multi-Harmonics_Monitoring_CT.php">VIEW MORE</a>
-                    </div>
-                </div>
-            </div>
-            <div class="content_sol_box ml_480_n">
-                <div class="sol_padding_box">
-                    <div class="sol_img sol_ct_06">
-                        <a href="/Solid_CT/High_Frequency_Monitoring_CT.php"><img src="/img/ctt_img_06.png" alt="ctt_img"></a>
-                    </div>
-                    <div class="sol_txt">
-                        <h4>High Frequency Monitoring CT</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    </div>
-                    <div class="sol_btn_box">
-                        <a class="sol_btn" href="/Solid_CT/High_Frequency_Monitoring_CT.php">VIEW MORE</a>
                     </div>
                 </div>
             </div>
